bc320a95a4a59d664aa17c65bf165c845a2654b8
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-22 16:41:56 +0200

    add studente

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var studente = {
     nome: "Marco",
     cognome: "Rossi",
-    eta: "20"
+    eta: 20,
 }
 
-console.log(nome.cognome.eta)
+console.log(studente["nome"] + " " + studente["cognome"])
 
 
 
@@ -12,18 +12,34 @@ var arrayClasse = [
     {
         nome: "Luca",
         cognome: "Bianchi",
-        eta: "22"
+        eta: 22
     },
 
     {
         nome: "Giovanni",
         cognome: "Verdi",
-        eta: "21"
+        eta: 21
     },
 
     {
         nome: "Andrea",
         cognome: "Neri",
-        eta: "19"
+        eta: 19
     }
 ]
+
+var nuovoStudente = {
+    "nome": prompt("Inserisci il tuo nome"),
+    "cognome": prompt("Inserisci il tuo cognome"),
+    "eta": prompt("Inserisci la tua età")
+}
+
+arrayClasse.push(nuovoStudente)
+
+console.log(arrayClasse[0]["nome"] + " " + arrayClasse[0]["cognome"])
+console.log(arrayClasse[1]["nome"] + " " + arrayClasse[1]["cognome"])
+console.log(arrayClasse[2]["nome"] + " " + arrayClasse[2]["cognome"])
+console.log(nuovoStudente["nome"] + " " + nuovoStudente["cognome"])
+
+console.log(arrayClasse)
+
