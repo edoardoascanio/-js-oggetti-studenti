5529852ae712b1c600fc0d34c30c8edc1fe9932f
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-22 14:51:00 +0200

    add array

---
+++ script.js
@@ -0,0 +1,29 @@
+var studente = {
+    nome: "Marco",
+    cognome: "Rossi",
+    eta : "20"
+}
+
+console.log(nome.cognome.eta)
+
+
+
+var arrayClasse = [
+    {
+        nome: "Luca",
+        cognome: "Bianchi",
+        eta : "20"
+    },
+
+    {
+        nome: "Giovanni",
+        cognome: "Verdi",
+        eta : "20"
+    },
+
+    {
+        nome: "Andrea",
+        cognome: "Neri",
+        eta : "20"
+    }
+]
